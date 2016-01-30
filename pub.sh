557785cbed2b9c92bd7ff534979f883285e09b7f
Yong Yang <yangoliver@gmail.com>
2016-01-31 00:37:05 +0800

    auto add gitcafe in a new env

--- pub.sh
+++ pub.sh
@@ -4,8 +4,11 @@ echo "publish to github page"
 git push -u origin
 
 echo "#################################################"
-#
-# git remote add gitcafe https://gitcafe.com/yangoliver/yangoliver.git
-#
+git remote -v | grep gitcafe
+if [ $? -ne 0 ]; then
+	echo "can't find gitcafe as a remote branch, adding gitcafe now..."
+	git remote add gitcafe https://gitcafe.com/yangoliver/yangoliver.git
+fi
+
 echo "publish to gitcafe page"
 git push -u gitcafe master:gitcafe-pages
