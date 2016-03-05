727a1df834875cbdcf47126719dbab68dd299de9
Yong Yang <yangoliver@gmail.com>
2016-03-05 10:02:46 +0800

    migrate gitcafe to coding

--- pub.sh
+++ pub.sh
@@ -7,8 +7,8 @@ echo "#################################################"
 git remote -v | grep gitcafe
 if [ $? -ne 0 ]; then
 	echo "can't find gitcafe as a remote branch, adding gitcafe now..."
-	git remote add gitcafe https://gitcafe.com/yangoliver/yangoliver.git
+	git remote add coding https://git.coding.net/yangoliver/yangoliver.git
 fi
 
 echo "publish to gitcafe page"
-git push -u gitcafe master:gitcafe-pages
+git push -u coding master:gitcafe-pages
