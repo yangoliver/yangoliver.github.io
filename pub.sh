a77f5f504bf36603ef4139caa2a961562964fc0c
Yong Yang <yangoliver@gmail.com>
2016-03-05 16:31:06 +0800

    fix for coding.net migration

--- pub.sh
+++ pub.sh
@@ -4,11 +4,11 @@ echo "publish to github page"
 git push -u origin
 
 echo "#################################################"
-git remote -v | grep gitcafe
+git remote -v | grep coding
 if [ $? -ne 0 ]; then
-	echo "can't find gitcafe as a remote branch, adding gitcafe now..."
+	echo "can't find gitcafe as a remote branch, adding coding branch for coding.net now..."
 	git remote add coding https://git.coding.net/yangoliver/yangoliver.git
 fi
 
-echo "publish to gitcafe page"
+echo "publish to coding.net page"
 git push -u coding master:gitcafe-pages
